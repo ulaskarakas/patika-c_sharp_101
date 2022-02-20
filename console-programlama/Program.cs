c2fa0c19083328524fce9b0cdec512bbe1949aa1
Ulas Karakas <ulaskarakas95@gmail.com>
2022-02-21 00:09:53 +0300

    patika - C#101 - console-programlama

---
+++ console-programlama/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace console_programlama // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.WriteLine("İsminizi giriniz:");
+            var name = Console.ReadLine();
+            //Console.WriteLine("İsminizi giriniz:");
+            var surname = Console.ReadLine();
+
+            //Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
