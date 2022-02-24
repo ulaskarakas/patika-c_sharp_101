e30ccd84fb307a9e4a88db5593e477b02029b53a
Ulas Karakas <ulaskarakas95@gmail.com>
2022-02-25 02:06:40 +0300

    patika - C#101 - hata-yonetimi

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmis oldugunuz sayi: " + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Islem tamamlandi.");
+            }
+            */
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Bos deger girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun degil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Cok kucuk ya da cok buyuk bir deger girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally 
+            {
+                Console.WriteLine("Islem basarili bir sekilde tamamlandi");
+            }
+
+        }
+    }
+}
